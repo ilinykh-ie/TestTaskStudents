757c58165de3040dc87dc65e8ea1bcdb97d7f74e
i.ilinykh <i.ilinykh@intabia.ru>
2025-11-19 15:16:16 +0700

    initial commit

---
+++ src/main/java/ru/ilinykh/testtaskstudents/TestTaskStudentsApplication.java
@@ -0,0 +1,13 @@
+package ru.ilinykh.testtaskstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskStudentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskStudentsApplication.class, args);
+    }
+
+}
